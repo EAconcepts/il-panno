8d28ad39636496dec8a40e9496108d7a48f850d4
EAconcepts <amooemmanuel10@gmail.com>
2023-11-06 21:12:12 +0100

    scaffoldolded the project

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RootLayout from "../components/layout/layout";
+
+const page = () => {
+  return (
+    // <main className="">
+      <RootLayout offerTop={true}>
+
+      </RootLayout>
+    // </main>
+  );
+};
+
+export default page;
